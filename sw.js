c81286aa32ef24f8ddfdcb9eb55a985105d9b3c3
itxArun <thanoslive135@gmail.com>
2026-05-12 10:23:08 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,8 @@
+self.addEventListener('install', (e) => {
+    console.log('[Crave AR] Service Worker Installed');
+});
+
+self.addEventListener('fetch', (e) => {
+    // Bas basic fetch logic taaki browser isko installable PWA maan le
+});
 
